489d2b74b208d370418e3a155388d7f4ded7c31e
Betrax <oguzalinrb@gmail.com>
2020-08-11 20:09:00 +0200

    test

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
 import requests
 
-url = "https://chicken-coop.p.rapidapi.com/games/%7Btitle%7D"
+url = "https://rawg-video-games-database.p.rapidapi.com/games"
 
-querystring = {"platform":"pc"}
+headers = {"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com", "x-rapidapi-key": "ccf1deb229msh7f504a4bb515fe7p170fb7jsnac008b4dba51"}
 
-headers = {
-    'x-rapidapi-host': "chicken-coop.p.rapidapi.com",
-    'x-rapidapi-key': "ccf1deb229msh7f504a4bb515fe7p170fb7jsnac008b4dba51"
-    }
+response = requests.request("GET", url, headers=headers).text
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-print(response.text)
+print(response)
